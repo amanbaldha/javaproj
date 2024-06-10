9795c7962f6d5f9371995685f5479412b3268372
amanbaldha <169175825+amanbaldha@users.noreply.github.com>
2024-06-10 09:38:41 +0530

    Add files via upload

---
+++ Animals/test.java
@@ -0,0 +1,11 @@
+
+package Animals;
+
+public class test {
+    public static void main(String args[]) {
+        Cats c = new Cats();
+        c.sound();
+        Dogs d = new Dogs();
+        d.sound();
+    }
+}
